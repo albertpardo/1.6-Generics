5157819b03574040fe6bc22703af304f5163b7e6
apm <apm@mail.com>
2025-11-20 17:51:06 +0100

    [T6L2E2] Done

--- src/level2ex1/GenericMethods.java
+++ src/level2ex1/GenericMethods.java
@@ -1,9 +1,10 @@
 package level2ex1;
 
 public class GenericMethods {
-    public static <T1, T2> void genericPrint(T1 varOne, T2 varTwo, boolean varThree){
-        System.out.println("T1 = '" + varOne + "'");
-        System.out.println("T2 = '" + varTwo + "'");
-        System.out.println("varThree = '" + varThree + "'" );
+    @SafeVarargs
+    public static <T> void genericPrint(T... arguments){
+        for (T argument : arguments){
+            System.out.println(argument);
+        }
     }
 }
