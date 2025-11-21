5c11d86fb9470528f0addbe91576b71561d8ebec
apm <apm@mail.com>
2025-11-21 13:52:58 +0100

    code reviewed

--- src/level1ex2/GenericMethods.java
+++ src/level1ex2/GenericMethods.java
@@ -2,8 +2,8 @@ package level1ex2;
 
 public class GenericMethods {
     public static <T1, T2, T3> void genericPrint(T1 varOne, T2 varTwo, T3 varThree){
-        System.out.println("T1 = '" + varOne + "'");
-        System.out.println("T2 = '" + varTwo + "'");
-        System.out.println("T3 = '" + varThree + "'" );
+        System.out.println("T1 -> {" + varOne + "}");
+        System.out.println("T2 -> {" + varTwo + "}");
+        System.out.println("T3 -> {" + varThree + "}" );
     }
 }
--- src/level2ex1/GenericMethods.java
+++ src/level2ex1/GenericMethods.java
@@ -1,9 +1,9 @@
 package level2ex1;
 
 public class GenericMethods {
-    public static <T1, T2, T3> void genericPrint(T1 varOne, T2 varTwo, boolean varThree){
-        System.out.println("T1 = '" + varOne + "'");
-        System.out.println("T2 = '" + varTwo + "'");
-        System.out.println("T3 = '" + varThree + "'" );
+    public static <T1, T2> void genericPrint(T1 varOne, T2 varTwo, boolean varThree){
+        System.out.println("T1 -> {" + varOne + "}");
+        System.out.println("T2 -> {" + varTwo + "}");
+        System.out.println("boolean var -> {" + varThree + "}" );
     }
 }
